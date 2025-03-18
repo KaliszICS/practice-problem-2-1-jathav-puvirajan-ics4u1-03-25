44c33a9b395e97b99eb84697ead3b451e3603d6c
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-03-18 19:02:54 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,30 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find(int[] arr, int n){
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == n) return i;
+		}
+		return -1;
+	}
+	public static int findLast(String[] arr, String n){
+		int o = -1;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == n) o = i;
+		}
+		return o;
+	}
+	public static int findSecond(char[] arr, char n){
+		int o = -1;
+		boolean foundFirst = false;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == n){
+				if(foundFirst) return i;
+				o = i;
+				foundFirst = true;
+			}
+		}
+		return o;
+	}	
 
 }
